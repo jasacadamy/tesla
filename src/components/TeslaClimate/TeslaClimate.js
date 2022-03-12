2af476b2d8cb7acb3149412cb046aa1daa087d7e
Anjali Kumari <anjaliaaa654@gmail.com>
2022-03-12 18:15:04 +0530

    first commit

--- src/components/TeslaClimate/TeslaClimate.js
+++ src/components/TeslaClimate/TeslaClimate.js
@@ -4,7 +4,8 @@ import './TeslaClimate.css';
 const TeslaClimate = (props) => (
   <div className="tesla-climate">
     <label
-      className={`tesla-climate__item ${props.value ? 'tesla-climate__item--active' : '' }  ${!props.limit ? 'tesla-heat':''}`}
+      className={`tesla-climate__item ${props.value ? 'tesla-climate__item--active' : '' } 
+       ${!props.limit ? 'tesla-heat':''}`}
     >
       <p>{props.limit ? 'ac' : 'heat'} {props.value ? 'on' : 'off'}</p>
       <i className="tesla-climate__icon"></i>
--- src/components/TeslaWheels/TeslaWheels.js
+++ src/components/TeslaWheels/TeslaWheels.js
@@ -6,18 +6,19 @@ const LabelLists = (props) => {
   const changeHandler = props.wheels.handleChangeWheels;
   const sizes = [19, 21];
   const LabelItems = sizes.map(size => (
-    <label key={size} className={`tesla-wheels__item tesla-wheels__item--${size} ${value === size ? 'tesla-wheels__item--active' : '' }`}>
+    <label key={size} className={`tesla-wheels__item tesla-wheels__item--${size} 
+    ${value === size ? 'tesla-wheels__item--active' : ''}`}>
       <input
         type="radio"
         name="wheelsize"
         value={size}
-        checked={value === size} 
-        onChange={() => {changeHandler(size)}} />
+        checked={value === size}
+        onChange={() => { changeHandler(size) }} />
       <p>
         {size}"
       </p>
-    </label> 
-    )
+    </label>
+  )
   );
   return (
     <div>
@@ -29,7 +30,7 @@ const TeslaWheels = (props) => (
   <div className="tesla-wheels__component">
     <p className="tesla-wheels__title">Wheels</p>
     <div className="tesla-wheels__container cf">
-      <LabelLists wheels={props}/>
+      <LabelLists wheels={props} />
     </div>
   </div>
 );
